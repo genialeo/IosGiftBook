3e2c62a0c8d4ae74e3eace5381de2e20dd4e91e5
Your Name <lfenu@amazon.com>
2021-04-16 16:53:06 +0200

    Initial Commit

---
+++ giftBook/giftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  giftBookApp.swift
+//  giftBook
+//
+//  Created by Fenu, Leonardo on 16/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct giftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
